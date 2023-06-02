2dec5d258f17d1695cf99d8d536f41e0f2d10bed
Elad Dadon <elaad2408@gmail.com>
2023-06-03 01:35:50 +0300

    continue work on the main container

--- src/app/components/portfolio/main_container/Index.tsx
+++ src/app/components/portfolio/main_container/Index.tsx
@@ -2,6 +2,9 @@
 import { Btn } from "../Btn/Index";
 import { Stats_box } from "../Stats_box/Index";
 import "./index.css";
+import Image from "next/image";
+import Stock from "../../../../../public/stock.png";
+
 export const Main_container: React.FC = () => {
   return (
     <div className="Main_container">
@@ -15,24 +18,28 @@ export const Main_container: React.FC = () => {
 
       <div className="second_row">
         <Stats_box
+          v1={true}
           finel_sum={42069}
           positibe={true}
           stat={24}
           title="my balance"
         />
         <Stats_box
+          v1={true}
           finel_sum={20619}
           positibe={true}
           stat={28}
           title="investment"
         />
         <Stats_box
+          v1={true}
           finel_sum={8664}
           positibe={true}
           stat={22}
           title="totoal gain"
         />
         <Stats_box
+          v1={true}
           finel_sum={1212}
           positibe={false}
           stat={20}
@@ -44,9 +51,27 @@ export const Main_container: React.FC = () => {
         <div className="first_part">
           <div>overview statistic</div>
           <div className="btns">
-            <span>&</span>
-            <span>^</span>
-            <span>@</span>
+            <Btn icon={true} document={true} />
+            <Btn icon={true} star={true} />
+            <Btn icon={true} gear={true} />
+          </div>
+        </div>
+        <div className="second_part">
+          <div className="card">
+            <Image
+              className="img"
+              src={Stock}
+              width={40}
+              height={40}
+              alt="stock icon"
+            />
+            <Stats_box
+              v2={true}
+              finel_sum={8664}
+              positibe={true}
+              stat={22}
+              title="origin game ea inc. (orea)"
+            />
           </div>
         </div>
       </div>

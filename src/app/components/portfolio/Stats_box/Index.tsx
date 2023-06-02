f2ac04d6dad9868330ba7fdca9bf6ab30e8c89f0
Elad Dadon <elaad2408@gmail.com>
2023-06-03 01:35:13 +0300

    added more stats_box  options

--- src/app/components/portfolio/Stats_box/Index.tsx
+++ src/app/components/portfolio/Stats_box/Index.tsx
@@ -7,7 +7,16 @@ export const Stats_box: React.FC<{
   stat: number;
   positibe: boolean;
   finel_sum: number;
-}> = ({ title, stat, positibe = true, finel_sum = 10000 }) => {
+  v1?: boolean;
+  v2?: boolean;
+}> = ({
+  title,
+  stat,
+  positibe = true,
+  finel_sum = 10000,
+  v1 = false,
+  v2 = false,
+}) => {
   const [finel_number, setFinel_number] = useState("0");
 
   useEffect(() => {
@@ -20,15 +29,36 @@ export const Stats_box: React.FC<{
   }, [finel_sum]);
   return (
     <div className="Stats_box">
-      <div className="top_row">
-        <div className="title">{title}</div>
-        {positibe ? (
-          <div className="posotive_stats">{stat}</div>
-        ) : (
-          <div className="negtive_stats">{stat}</div>
-        )}
-      </div>
-      <div className="finel_sum">{finel_number}</div>
+      {v1 ? (
+        <>
+          <div className="top_row">
+            <div className="title">{title}</div>
+            {positibe ? (
+              <div className="posotive_stats">{stat}</div>
+            ) : (
+              <div className="negtive_stats">{stat}</div>
+            )}
+          </div>
+          <div className="finel_sum">{finel_number}</div>
+        </>
+      ) : v2 ? (
+        <>
+          <div className="top_row">
+            <div className="title">{title}</div>
+          </div>
+          <div className="bottom_row">
+
+            {positibe ? (
+              <div className="posotive_stats">{stat}</div>
+              ) : (
+                <div className="negtive_stats">{stat}</div>
+                )}
+          <div className="finel_sum">{finel_number}</div>
+                </div>
+        </>
+      ) : (
+        ""
+      )}
     </div>
   );
 };

b47f866f363b7820b4367da4b192403d1fc33952
Elad Dadon <elaad2408@gmail.com>
2023-06-02 18:54:58 +0300

    comtinue work

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import "./index.css";
-
+import { Main_container } from "../components/portfolio/main_container/Index";
+import { Second_container } from "../components/portfolio/Second_container/Index";
 export default function Portfolio() {
-  return <div className="portfolio">page portfolio</div>;
+  return (
+    <div className="portfolio">
+      <Main_container />
+      <Second_container/>
+    </div>
+  );
 }

df370bd375d1f76990cc1435a74fc1aee8f4103a
Elad Dadon <elaad2408@gmail.com>
2023-06-18 01:37:46 +0300

    change the main forlder of git location

--- src/app/components/HeaderColor/Index.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import "./index.css";
-
-export const HeaderColor:React.FC= ()=> {
-  return(
-    <div className="HeaderColor"></div>
-  )
-}
--- src/servises/3rd_api.js
+++
@@ -1,5 +0,0 @@
-
-
-export const current_weather=(lat,lon)=>{
-    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5a1b390e336b7838dbcfb576be18d6bf`
-};
